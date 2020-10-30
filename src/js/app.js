ffc3b3d36f26f483746c02182e8d12f71f9ef617
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-10-30 01:27:43 -0500

    Using local storage as mock DB

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,25 @@
+const addToStorage = (date, startTime, endTime, subEntries) => {
+  localStorage.setItem(
+    date,
+    JSON.stringify({
+      start: startTime,
+      end: endTime,
+      "sub-entries": subEntries,
+    })
+  );
+};
+
+const removeFromStorage = (date) => {
+  localStorage.removeItem(date);
+};
+
 // Add a new entry on click
-// To-Do: change add to use mock DB
 const addEntry = () => {
   const li = document.createElement("li");
   const inputValDate = document.getElementById("date-input").value;
   const text = document.createTextNode(inputValDate);
   const span = document.createElement("span");
+  const remove = document.getElementsByClassName("remove-btn");
 
   li.appendChild(text);
 
@@ -16,21 +31,41 @@ const addEntry = () => {
     span.innerHTML = "X";
     li.appendChild(span);
     document.getElementById("date-input").value = "";
+    addToStorage(inputValDate, "start", "end", []);
   }
 
   for (let i = 0; i < remove.length; i++) {
     remove[i].onclick = function () {
       this.parentElement.style.display = "none";
+      removeFromStorage(this.parentElement.innerText.replace("X", ""));
     };
   }
 };
 
-// Click on X to hide entry
-// To-Do: replace hide with remove from mock DB
-const remove = document.getElementsByClassName("remove-btn");
+// Retrieve current state of entry list from storage and display it
+const getCurrList = () => {
+  Object.keys(localStorage).forEach((date) => {
+    const li = document.createElement("li");
+    const text = document.createTextNode(date);
+    const span = document.createElement("span");
+    const remove = document.getElementsByClassName("remove-btn");
 
-for (let i = 0; i < remove.length; i++) {
-  remove[i].onclick = function () {
-    this.parentElement.style.display = "none";
-  };
-}
+    li.appendChild(text);
+    document.getElementById("list").appendChild(li);
+    span.className = "remove-btn";
+    span.innerHTML = "X";
+    li.appendChild(span);
+    document.getElementById("date-input").value = "";
+
+    for (let i = 0; i < remove.length; i++) {
+      remove[i].onclick = function () {
+        this.parentElement.style.display = "none";
+        removeFromStorage(this.parentElement.innerText.replace("X", ""));
+      };
+    }
+  });
+};
+
+const printLatest = () => {
+  console.log(localStorage);
+};
