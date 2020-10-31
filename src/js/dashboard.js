2257d3025ca04620d10c6cda6e53b122f723c0a3
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-10-31 15:45:06 -0500

    Date util

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,3 +1,34 @@
 const printLatest = () => {
   console.log(localStorage);
+  getAllDates();
+};
+
+const getAllDates = () => {
+  Object.entries(localStorage).forEach((entry) => {
+    const taskList = JSON.parse(entry[1]).taskList;
+    console.log(convertDate(entry[0]));
+  });
+};
+
+const convertDate = (date) => {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const result = new Date(date);
+
+  return `${days[result.getUTCDay()]}, ${result.getUTCDate()} ${
+    months[result.getUTCMonth()]
+  } ${result.getUTCFullYear()}`;
 };
