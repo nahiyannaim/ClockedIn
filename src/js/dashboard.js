0d3e2b0ba3837f1ac61f7546e1d41946d1cf8fd4
Nahiyan Naim <nahiyannaim@yahoo.com>
2020-11-01 19:03:29 -0600

    Chart visual tweak

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -95,14 +95,14 @@ const displayBarChart = (data) => {
     .keys();
 
   // Add X axis
-  let x = d3.scaleBand().domain(groups).range([0, width]).padding([0.2]);
+  let x = d3.scaleBand().domain(groups).range([0, width]).padding([0.35]);
   svg
     .append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x).tickSizeOuter(0));
 
   // Add Y axis
-  let y = d3.scaleLinear().domain([0, 60]).range([height, 0]);
+  let y = d3.scaleLinear().domain([0, 40]).range([height, 0]);
   svg.append("g").call(d3.axisLeft(y));
 
   // One color per subgroup
